bdf2b60b0bb05e8269b78f7171cefd870c5b27e4
Megan Wu <32400725+mwu00@users.noreply.github.com>
2018-03-31 16:34:54 -0700

    Updated MCDB scraping script
    Added DataFrame feature; minor debugging for list of dates

--- python/scrape_mcdb.py
+++ python/scrape_mcdb.py
@@ -1,9 +1,13 @@
 # import library used to query website
 # import BeautifulSoup functions to parse data returned from website
 # import date parser
+# import pandas to convert list to DataFrame
+# import numpy for NaN values in DataFrame
 import urllib.request
 from bs4 import BeautifulSoup
 from dateutil import parser
+import pandas as pd
+import numpy as np
 
 my_site = "https://www.mcdb.ucla.edu/seminars"
 my_site_base = "https://www.mcdb.ucla.edu"
@@ -13,8 +17,8 @@ my_site_base = "https://www.mcdb.ucla.edu"
 orig_html = urllib.request.urlopen(my_site)
 
 # parse HTML in the orig_html variable and store in BeautifulSoup format
-# 'lxml' = HTML parser recommended by compiler
-parsed_page = BeautifulSoup(orig_html, 'lxml')
+# 'html5lib' = HTML parser recommended by compiler
+parsed_page = BeautifulSoup(orig_html, 'html5lib')
 
 # begin separating titles, speakers, dates/times, locations
 all_titles = parsed_page.find_all('div', class_='views-field views-field-title')
@@ -53,11 +57,10 @@ for chunk in all_speakers:
 # process dates and times
 list_dates = []
 list_start_times = []
-list_end_times = []
 
 for chunk in all_times:
 	for x in chunk.findAll('span', class_='date-display-single'):
-		my_date = parser.parse(x['content']).now().date()
+		my_date = parser.parse(x['content']).date()
 		list_dates.append(my_date.isoformat())
 		
 		my_time = parser.parse(x['content']).time()
@@ -72,14 +75,33 @@ for chunk in all_locations:
 		list_locations.append(x.string)
 		
 
-print(list_links)
-print(list_titles)
-print(list_speakers)
-print(list_locations)
+# create DataFrame
+info_df = pd.DataFrame(
+	{'link': list_links,
+	'title': list_titles,
+	'speaker': list_speakers,
+	'location': list_locations,
+	'date': list_dates,
+	'start-time': list_start_times,
+	'end-time': np.nan,
+	'subject': 'MCDB',
+	'type': 'seminar'})
+	
+# preserve column order
+info_df = info_df[['link', 'title', 'speaker', 'location', 'date', 'start-time', 'end-time', 'subject', 'type']]
+	
+	
+# 
+# print(list_links)
+# print(list_titles)
+# print(list_speakers)
+# print(list_locations)
+# 
+# print(list_dates)
+# print(list_start_times)
+# 
 
-print(list_dates)
-print(list_start_times)
-print(list_end_times)
+print(info_df)
 
 		
 
